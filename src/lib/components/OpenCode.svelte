30dc84bcd7769ca93ef1b856d0ff7d42c30043bf
selemondev <106826371+selemondev@users.noreply.github.com>
2026-03-13 00:39:44 +0000

    chore: update svgl svelte components
    Added 4 new components: - Dracula
    - OpenClaw
    - OpenCode
    - Vlt
    
    Generation time: 0m 27s

---
+++ src/lib/components/OpenCode.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	export interface Props extends HTMLAttributes<SVGElement> {
+		width?: number;
+		height?: number;
+	}
+
+	let { width = 50, height = 50, ...rest }: Props = $props();
+	const restAttrs = Object.entries(rest)
+		.map(([k, v]) => `${k}="${v}"`)
+		.join(' ');
+	const svgContent = `
+<svg viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" ${restAttrs}>
+<rect width="512" height="512" fill="#FDFCFC"/>
+<path d="M320 224V352H192V224H320Z" fill="#E6E5E6"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M384 416H128V96H384V416ZM320 160H192V352H320V160Z" fill="#17181C"/>
+</svg>`;
+</script>
+
+{@html svgContent}
